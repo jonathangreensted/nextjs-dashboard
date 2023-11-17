45266fb6c10f96739d921c01b16738f21c85344a
Jonathan Greensted <jonathan@greensted.com>
2023-11-17 15:22:54 +0000

    Fixed the null invoice bug

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,9 +27,9 @@ export async function createInvoice(formData: FormData) {
     const date = new Date().toISOString().split('T')[0];
 
     await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
